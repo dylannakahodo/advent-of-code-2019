4314b75b5e2f2cdf07e6fc2c6fe7f636dab6c7bf
Dylan Nakahodo <dylannakahodo@gmail.com>
2019-12-07 14:08:47 -0500

    Day 3 Part 1 Done

---
+++ day-3-crossed-wires/one.py
@@ -0,0 +1,36 @@
+with open('input.txt') as f:
+    wire_1_path = f.readline().strip().split(',')
+    wire_2_path = f.readline().strip().split(',')
+
+def manhattan_distance(point):
+    return abs(point[0]) + abs(point[1])
+
+def parse_wire_positions(wire_path):
+    x,y = 0,0
+    wire_positions = set()
+
+    for vector in wire_path:
+        for _ in range(int(vector[1:])):
+            direction = vector[0]
+
+            if direction == "R":
+                x += 1
+            elif direction == "L":
+                x -= 1
+            elif direction == "U":
+                y += 1
+            elif direction == "D":
+                y -= 1
+            
+            wire_positions.add((x,y))
+            
+    return wire_positions
+
+wire_1_points = parse_wire_positions(wire_1_path)
+wire_2_points = parse_wire_positions(wire_2_path)
+
+intersections = wire_1_points.intersection(wire_2_points)
+
+closest = min(map(manhattan_distance,intersections))
+
+print(closest)
